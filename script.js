32fc98e2d5607615f8131586d939cdc3b0dccb83
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-01-12 13:22:03 -0300

    Add new book logic with DOM manipulation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const display = document.querySelector('.display-container');
+const addBookBtn = document.querySelector('.add-book-btn');
+addBookBtn.addEventListener('click', () => {
+  // Append form to add a book
+  addBook('The Hobbit', 'J.R.R. Tolkien', 295, true);
+  addBook('The Martian', 'Andy Weir', 369, true);
+});
+
 const library = [];
 
 function Book(title, author, pages, haveRead) {
@@ -5,13 +13,50 @@ function Book(title, author, pages, haveRead) {
   this.author = author;
   this.pages = pages;
   this.haveRead = haveRead;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${haveRead ? 'read' : 'not read yet'}`;
-  }
 }
 
-function addBookToLibrary(title, author, pages, haveRead) {
+function addBook(title, author, pages, haveRead) {
   const book = new Book(title, author, pages, haveRead);
   library.push(book);
+  updateDisplay();
 }
 
+function removeBook(index) {
+
+}
+
+function toggleRead() {
+
+}
+
+function updateDisplay() {
+  display.innerHTML = "";
+
+  // Function to update the books in the display contianer
+  library.forEach((book, index) => {
+    const bookCard = createBookCard(book);
+    display.appendChild(bookCard);
+  });
+}
+
+function createBookCard(book) {
+  const bookCard = document.createElement('div');
+  bookCard.className = 'book-card';
+
+  const bookTitle = document.createElement('h2');
+  bookTitle.className = 'book-title';
+  bookTitle.textContent = book.title;
+  const bookAuthor = document.createElement('h3');
+  bookAuthor.className = 'book-author';
+  bookAuthor.textContent = book.author
+  const bookPages = document.createElement('p');
+  bookPages.className = 'book-pages';
+  bookPages.textContent = book.pages;
+  const bookRead = document.createElement('p');
+  bookRead.className = 'book-read';
+  bookRead.textContent = book.haveRead ? 'Read' : 'Not read yet';
+
+  bookCard.append(bookTitle, bookAuthor, bookPages, bookRead);
+
+  return bookCard;
+}
