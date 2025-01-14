1b7a154230a70f81e51fe2beea8fe9031709ee6d
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-01-14 00:48:24 -0300

    Add full layout to create book card with event handlers

--- script.js
+++ script.js
@@ -6,24 +6,6 @@ addBookBtn.addEventListener('click', () => {
   addBook('The Martian', 'Andy Weir', 369, true);
 });
 
-const btnRead = document.querySelector('.btn-read');
-btnRead.addEventListener('mouseover', () => {
-  // check read state before changing icon
-});
-
-btnRead.addEventListener('mouseout', () => {
-  // check read state before changing icon
-});
-
-const btnRemove = document.querySelector('.btn-remove');
-btnRemove.addEventListener('mouseover', () => {
-  btnRemove.children[0].setAttribute('src', './icons/delete-red.svg');
-});
-
-btnRemove.addEventListener('mouseout', () => {
-  btnRemove.children[0].setAttribute('src', './icons/delete.svg');
-});
-
 const library = [];
 
 function Book(title, author, pages, haveRead) {
@@ -64,17 +46,60 @@ function createBookCard(book) {
   const bookTitle = document.createElement('h2');
   bookTitle.className = 'book-title';
   bookTitle.textContent = book.title;
+  
   const bookAuthor = document.createElement('h3');
   bookAuthor.className = 'book-author';
   bookAuthor.textContent = book.author
+  
   const bookPages = document.createElement('p');
   bookPages.className = 'book-pages';
   bookPages.textContent = `${book.pages} pages`;
+
   const bookRead = document.createElement('p');
   bookRead.className = 'book-read';
   bookRead.textContent = book.haveRead ? 'Read' : 'Not read yet';
 
-  bookCard.append(bookTitle, bookAuthor, bookPages, bookRead);
+  const readBtn = document.createElement('span');
+  readBtn.classList.add('btn-read', 'icon-btn');
+  readBtn.addEventListener('mouseover', mouseOverRead);
+  readBtn.addEventListener('mouseout', mouseOutRead);
+
+  const readIcon = document.createElement('img');
+  readIcon.setAttribute('src', './icons/eye-plus.svg');
+  readIcon.className = 'icon-read';
+  
+  readBtn.appendChild(readIcon);
+
+  const removeBtn = document.createElement('span');
+  removeBtn.classList.add('btn-remove', 'icon-btn');
+  removeBtn.addEventListener('mouseover', mouseOverRemove);
+  removeBtn.addEventListener('mouseout', mouseOutRemove);
+
+  const removeIcon = document.createElement('img');
+  removeIcon.setAttribute('src', './icons/delete.svg');
+  removeIcon.className = 'icon-remove';
+
+  removeBtn.appendChild(removeIcon);
+
+  bookCard.append(bookTitle, bookAuthor, bookPages, bookRead, readBtn, removeBtn);
 
   return bookCard;
+}
+
+// --- Handle btnRead events ---
+function mouseOverRead() {
+  // If read state is false
+  this.children[0].setAttribute('src', './icons/eye-plus-green.svg');
+}
+
+function mouseOutRead() {
+  this.children[0].setAttribute('src', './icons/eye-plus.svg');
+}
+
+function mouseOverRemove() {
+  this.children[0].setAttribute('src', './icons/delete-red.svg');
+}
+
+function mouseOutRemove() {
+  this.children[0].setAttribute('src', './icons/delete.svg');
 }
